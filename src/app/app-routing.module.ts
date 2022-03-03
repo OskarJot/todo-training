e86f3c183fb477bac0b72a01d232aea039fb8396
OskarJot <oskar.jarnot@gmail.com>
2022-03-03 19:55:17 +0100

    new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OmniePageModule } from './pages/omnie.page-module';
+import { MojastronaPageModule } from './pages/mojastrona.page-module';
+import { KontaktPageModule } from './pages/kontakt.page-module';
 
 const routes: Routes = [{ 
-        path: '', 
+        path: 'Omnie', 
         loadChildren: () => OmniePageModule
-      }];
+      },
+  { 
+        path: 'mojastrona', 
+        loadChildren: () => MojastronaPageModule
+      },
+  { 
+        path: 'kontakt', 
+        loadChildren: () => KontaktPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
