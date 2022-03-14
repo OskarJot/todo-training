c1acef059e7c4cc107fd3f428cb7c4daf17093e8
OskarJot <oskar.jarnot@gmail.com>
2022-03-14 14:17:41 +0100

    Add formularz, przekazywanie informacji do konsoli

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -3,11 +3,21 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
 } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'lib-contact-form',
   templateUrl: './contact-form.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  readonly messageForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    text: new FormControl(),
+  });
+
+  onFormSubmited(employeeForm: FormGroup) {
+    {console.log(this.messageForm.getRawValue());}
+}
+
+}
--- projects/team/src/lib/adapters/primary/ui/our-team.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OurTeamComponent } from './our-team.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({ imports: [CommonModule, RouterModule],
-  	declarations: [OurTeamComponent],
-  	providers: [],
-  	exports: [OurTeamComponent] })
-export class OurTeamComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  declarations: [OurTeamComponent],
+  providers: [],
+  exports: [OurTeamComponent],
+})
+export class OurTeamComponentModule {}
