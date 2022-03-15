bc50455fedfa8831889e0351f4317f347c079c79
OskarJot <oskar.jarnot@gmail.com>
2022-03-15 23:42:13 +0100

    Error: Implementacja DTO Port + lowgular/shared

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -17,20 +17,23 @@ export class OurTeamComponent {
     {
       id: '1',
       name: 'Jan Frodeno',
-      imageUrl: 'https://akademiatriathlonu.pl/wp-content/uploads/2019/10/Ironman-Kona-PRO-m%C4%99%C5%BCczy%C5%BAni-mini.jpg',
-      department: {bio: 'PRO TRI'},
+      imageUrl:
+        'https://akademiatriathlonu.pl/wp-content/uploads/2019/10/Ironman-Kona-PRO-m%C4%99%C5%BCczy%C5%BAni-mini.jpg',
+      department: { bio: 'PRO TRI' },
     },
     {
       id: '2',
       name: 'Mark Allen',
-      imageUrl: 'https://www.triathlon.org/images/news_thumbs/Avignon_1989_-_Mark_Allen_-_credit_ITU.jpg',
-      department: {bio: 'PRO TRI'},
+      imageUrl:
+        'https://www.triathlon.org/images/news_thumbs/Avignon_1989_-_Mark_Allen_-_credit_ITU.jpg',
+      department: { bio: 'PRO TRI' },
     },
     {
       id: '3',
       name: 'Jerzy Górski',
-      imageUrl: 'http://ocdn.eu/images/pulscms/ZWY7MDA_/d3d70566e584f7cc91fefd746961cea2.jpeg',
-      department: {bio: 'PRO TRI'},
+      imageUrl:
+        'http://ocdn.eu/images/pulscms/ZWY7MDA_/d3d70566e584f7cc91fefd746961cea2.jpeg',
+      department: { bio: 'PRO TRI' },
     },
   ]);
 
