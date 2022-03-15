9ed263285fe671d1455058420c13f805672670a2
OskarJot <oskar.jarnot@gmail.com>
2022-03-15 15:53:19 +0100

    Add navbar lib + smartkomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { OurTeamComponentModule } from 'projects/team/src/lib/adapters/primary/ui/our-team.component-module';
-
+import { NavbarComponentModule } from 'projects/navigation/src/lib/adapters/primary/ui/navbar.component-module';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,6 +16,7 @@ import { OurTeamComponentModule } from 'projects/team/src/lib/adapters/primary/u
     AppRoutingModule,
     OurTeamComponentModule,
     BrowserAnimationsModule,
+    NavbarComponentModule,
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
   ],
