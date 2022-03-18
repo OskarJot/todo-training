5760a2c2160122eac87a555cba3f330f369ee4f2
OskarJot <oskar.jarnot@gmail.com>
2022-03-18 18:47:36 +0100

    Dodana libka do admin pages

--- src/app/pages/admin.page-module.ts
+++ src/app/pages/admin.page-module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AdminPage } from './admin.page';
+import { CreateTriathletComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/create-triathlet.component-module';
+import { FirebaseAddTriathlonistListServiceModule } from '../../../projects/team/src/lib/adapters/secondary/infrastructure/firebase-add-triathlonist-list.service-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +11,10 @@ import { AdminPage } from './admin.page';
           path: '',
           component: AdminPage,
         }
-      ])],
+      ]),
+  CreateTriathletComponentModule,
+  FirebaseAddTriathlonistListServiceModule
+],
   	declarations: [AdminPage],
   	providers: [],
   	exports: [] })
