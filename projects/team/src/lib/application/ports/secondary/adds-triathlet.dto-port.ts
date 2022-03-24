b108ee995e95ca2956de66ec02f551d09ffbfc51
OskarJot <oskar.jarnot@gmail.com>
2022-03-24 08:59:55 +0100

    Porządki c.d

--- projects/team/src/lib/application/ports/secondary/adds-triathlet.dto-port.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { TriathletDTO } from './triathlet.dto';
-
-export const ADDS_TRIATHLET_DTO = new InjectionToken<AddsTriathletDtoPort>('ADDS_TRIATHLET_DTO');
-
-export interface AddsTriathletDtoPort {
-  add(triathlet: Partial<TriathletDTO>): void;
-}
