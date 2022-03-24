ca86e5eadfe02436a170ce3926d6166a48fec161
OskarJot <oskar.jarnot@gmail.com>
2022-03-24 11:00:54 +0100

    Lesson 7 - done / dodany provide

--- projects/team/src/lib/adapters/secondary/infrastructure/in-memory-context.storage-module.ts
+++ projects/team/src/lib/adapters/secondary/infrastructure/in-memory-context.storage-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CONTEXT_DTO_STORAGE } from '../../../application/ports/secondary/context-dto.storage-port';
 import { InMemoryContextStorage } from './in-memory-context.storage';
 
 @NgModule({ imports: [],
   	declarations: [],
-  	providers: [InMemoryContextStorage],
+  	providers: [InMemoryContextStorage,
+		{provide: CONTEXT_DTO_STORAGE, useExisting: InMemoryContextStorage},
+	],
   	exports: [] })
 export class InMemoryContextStorageModule {
 }
